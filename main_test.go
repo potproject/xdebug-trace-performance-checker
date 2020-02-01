72c648e694fb7cfe841e90c9687e6130aed3440e
potproject <pptppctt@gmail.com>
2020-02-01 22:20:40 +0900

    add test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -35,5 +36,53 @@ func TestDepth(t *testing.T) {
 	}
 	if depthPadding(10) != strings.Repeat(s, 23) {
 		t.Errorf("invalid depthPadding")
+	}
+}
+
+func TestParseTraceSuccess(t *testing.T) {
+	expect := []string{
+		"TRACE START [2020-02-01 12:30:32]",
+		"    0.0050      68952   -> {main}() /var/www/test.php:0",
+		"    0.0100     240056     -> include(/var/www/test2.php) /var/www/test.php:3",
+		"    0.0295     409624     -> test() /var/www/test.php:6",
+		"    0.1962     410440       -> foo() /var/www/test2.php:10",
+		"    0.3201     410472     -> test2() /var/www/test.php:7",
+		"    0.3211     410568       -> bar() /var/www/test2.php:24",
+	}
+	actual := []Trace{
+		Trace{time: 0.005, usedMemory: 68952, depth: 0, method: "{main}()", filePath: "/var/www/test.php", line: 0},
+		Trace{time: 0.01, usedMemory: 240056, depth: 1, method: "include(/var/www/test2.php)", filePath: "/var/www/test.php", line: 3},
+		Trace{time: 0.0295, usedMemory: 409624, depth: 1, method: "test()", filePath: "/var/www/test.php", line: 6},
+		Trace{time: 0.1962, usedMemory: 410440, depth: 2, method: "foo()", filePath: "/var/www/test2.php", line: 10},
+		Trace{time: 0.3201, usedMemory: 410472, depth: 1, method: "test2()", filePath: "/var/www/test.php", line: 7},
+		Trace{time: 0.3211, usedMemory: 410568, depth: 2, method: "bar()", filePath: "/var/www/test2.php", line: 24}}
+	result := parseTrace(expect)
+	if !reflect.DeepEqual(result, actual) {
+		t.Errorf("parseTrace() = %v, want %v", result, actual)
+	}
+}
+
+func TestParseTraceIrregular(t *testing.T) {
+	expect := []string{
+		"TRACE START [2020-02-01 12:30:32]",
+		"    0.0050      68952   -> {main}() /var/www/test.php:0",
+		" invalid string",
+		"    0.0100     240056     -> include(/var/www/test2.php) /var/www/test.php:3",
+		"    0.0295     409624     -> test() /var/www/test.php:6",
+		"    0.1962     410440       -> foo() /var/www/test2.php:10",
+		"    0.3201     410472     -> test2() /var/www/test.php:7",
+		"    0.3211     410568       -> bar() /var/www/test2.php:24",
+		"",
+	}
+	actual := []Trace{
+		Trace{time: 0.005, usedMemory: 68952, depth: 0, method: "{main}()", filePath: "/var/www/test.php", line: 0},
+		Trace{time: 0.01, usedMemory: 240056, depth: 1, method: "include(/var/www/test2.php)", filePath: "/var/www/test.php", line: 3},
+		Trace{time: 0.0295, usedMemory: 409624, depth: 1, method: "test()", filePath: "/var/www/test.php", line: 6},
+		Trace{time: 0.1962, usedMemory: 410440, depth: 2, method: "foo()", filePath: "/var/www/test2.php", line: 10},
+		Trace{time: 0.3201, usedMemory: 410472, depth: 1, method: "test2()", filePath: "/var/www/test.php", line: 7},
+		Trace{time: 0.3211, usedMemory: 410568, depth: 2, method: "bar()", filePath: "/var/www/test2.php", line: 24}}
+	result := parseTrace(expect)
+	if !reflect.DeepEqual(result, actual) {
+		t.Errorf("parseTrace() = %v, want %v", result, actual)
 	}
 }
